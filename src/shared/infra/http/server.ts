0f4bbcc321f2b403aaf55c71bf5fbe51e3b6f988
Leandro Moraes <leandromoraes12345@outlook.com>
2022-09-26 09:48:44 -0300

    :sparkles: add env config

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,8 @@
 import "reflect-metadata";
 import "module-alias/register";
 
+import "dotenv/config";
+
 import { server } from "./app";
 
 server.listen(process.env.PORT || 3333, () => {
--- src/shared/infra/mongoose/index.ts
+++ src/shared/infra/mongoose/index.ts
@@ -1,6 +1,3 @@
 import { connect } from "mongoose";
 
-connect(
-  "mongodb+srv://meruem:meruem44@learning.xwbwtbv.mongodb.net/?retryWrites=true&w=majority",
-  {}
-);
+connect(process.env.URL_DATABASE!, {});
